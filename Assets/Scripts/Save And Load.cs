f1a21177399bb79f53c7fb0800b27cd7c639f315
David Ung <david.ung@learnet.se>
2024-10-10 10:01:40 +0200

    Save and Load

---
+++ Assets/Scripts/Save And Load.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+
+public class SaveAndLoad : MonoBehaviour
+{
+    public CookieGameScript GameManager;
+
+   public void SaveGame()
+    {
+
+    }
+
+    public void LoadGame()
+    { 
+    
+    }
+}

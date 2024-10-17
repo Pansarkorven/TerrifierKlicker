6cce67586962c15932bc199170163b32329405de
Otto hammarberg <Otto.hammarberg@elev.lbs.ga.se>
2024-10-17 09:50:31 +0200

    Skin Manager

--- Assets/Scripts/SkinManager.cs
+++ Assets/Scripts/SkinManager.cs
@@ -3,44 +3,67 @@ using System.Collections.Generic;
 
 public class SkinManager : MonoBehaviour
 {
-   
     public List<int> skinUnlockThresholds = new List<int> { 100, 500, 1000, 5000, 10000 };
-
-    
     public List<GameObject> skins;
 
-    private int currentSkinIndex = 0; 
+    private int currentSkinIndex = 0;
 
     
-    public void CheckForSkinUnlock(int killCount)
+    void Start()
     {
-        if (currentSkinIndex < skinUnlockThresholds.Count && killCount >= skinUnlockThresholds[currentSkinIndex])
+
+        if (skins.Count > 0)
         {
-            UnlockNextSkin();
+            SetActiveSkin(0); 
+            Debug.Log("Base skin (skin 0) activated at the start of the game.");
+        }
+        else
+        {
+            Debug.LogWarning("No skins are assigned to the SkinManager.");
         }
     }
 
-    
+    public void CheckForSkinUnlock(int killCount)
+    {
+        Debug.Log($"Checking for skin unlock. Current kill count: {killCount}, current skin index: {currentSkinIndex}");
+
+        if (currentSkinIndex < skinUnlockThresholds.Count && killCount >= skinUnlockThresholds[currentSkinIndex])
+        {
+            Debug.Log($"Unlocking skin. Kill count ({killCount}) has reached the threshold for skin index {currentSkinIndex}.");
+            UnlockNextSkin();
+        }
+        else
+        {
+            Debug.Log($"No skin to unlock yet. Next unlock at {skinUnlockThresholds[currentSkinIndex]} kills.");
+        }
+    }
+
     private void UnlockNextSkin()
     {
         currentSkinIndex++;
         if (currentSkinIndex < skins.Count)
         {
+            Debug.Log($"Unlocking next skin. Setting skin index {currentSkinIndex} active.");
             SetActiveSkin(currentSkinIndex);
         }
         else
         {
-            Debug.LogWarning("No more skins to unlock!");
+            Debug.LogWarning("No more skins to unlock. All skins have been unlocked.");
         }
     }
 
-    
     private void SetActiveSkin(int index)
     {
+        Debug.Log($"Activating skin index: {index}");
+
         for (int i = 0; i < skins.Count; i++)
         {
-            skins[i].SetActive(i == index);
+            bool isActive = (i == index);
+            skins[i].SetActive(isActive);
+
+            Debug.Log($"Skin {i} active: {isActive}");
         }
-        Debug.Log($"Unlocked new skin: {index}");
+
+        Debug.Log($"Skin {index} has been set active.");
     }
 }

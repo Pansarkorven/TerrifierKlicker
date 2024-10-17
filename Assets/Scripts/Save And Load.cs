172703e6fd4e0eef71404934e78515da28864d68
David Ung <david.ung@learnet.se>
2024-10-17 08:47:17 +0200

    erm

--- Assets/Scripts/Save And Load.cs
+++ Assets/Scripts/Save And Load.cs
@@ -2,16 +2,31 @@ using UnityEngine;
 
 public class SaveAndLoad : MonoBehaviour
 {
-    public CookieGameScript GameManager;
+    public CookieGameScript gameManager;
 
    public void SaveGame()
     {
-        
+     //   PlayerPrefs.SetFloat("Kills", gameManager.);
+      //  PlayerPrefs.SetFloat("PassiveIncome", gameManager.);            
+   //     PlayerPrefs.SetInt("UpgradesBought", gameManager.);
+
+        PlayerPrefs.Save(); 
+        Debug.Log("Game Saved!");
 
     }
 
     public void LoadGame()
-    { 
-    
+    {
+      
+        {
+        //    gameManager.currency = PlayerPrefs.GetFloat("Currency", 0); 
+        //    gameManager.passiveIncome = PlayerPrefs.GetFloat("PassiveIncome", 1);
+         //   gameManager.upgradesBought = PlayerPrefs.GetInt("UpgradesBought", 0);
+
+            Debug.Log("Game Loaded!");
+        }
     }
+
+
 }
+
